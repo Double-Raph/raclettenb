771195df5739ec2fe0cc516fab1dbbdeb1536128
alban bengounia <alban.bengounia@gmail.com>
2024-12-03 15:47:59 +0100

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  has_many :raclettes, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, :address, presence: true
 end
