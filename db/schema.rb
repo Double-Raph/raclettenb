31b953511923d621afa5c9086592c5c63f5ec40c
Pauline Metay <metaypauline@gmail.com>
2024-12-03 18:02:20 +0100

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_03_153327) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_03_162918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "bookings", force: :cascade do |t|
-    t.datetime "start_date"
-    t.datetime "end_date"
-    t.string "status"
-    t.bigint "user_id", null: false
-    t.bigint "raclette_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["raclette_id"], name: "index_bookings_on_raclette_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +42,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_153327) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "raclette_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["raclette_id"], name: "index_bookings_on_raclette_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "raclettes", force: :cascade do |t|
     t.string "category"
     t.integer "capacity"
@@ -80,9 +80,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_03_153327) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "bookings", "raclettes"
-  add_foreign_key "bookings", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "raclettes"
+  add_foreign_key "bookings", "users"
   add_foreign_key "raclettes", "users"
 end
