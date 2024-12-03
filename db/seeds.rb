4a125c7b072fabf1e661b48e03118f27dde02ae0
raphael canches <raphael.canches@gmail.com>
2024-12-03 16:05:44 +0100

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+Raclette.destroy_all
+User.destroy_all
+
+
+users = []
+5.times do |i|
+  users << User.create!(
+    email: "user#{i + 1}@gmail.com",
+    password: "aaaaaa#{i + 1}",
+    password_confirmation: "aaaaaa#{i + 1}"
+  )
+end
+
+5.times do |i|
+  Raclette.create!(
+    type: Raclette::CATEGORIES.sample,
+    capacity: rand(2..20),
+    user: users.sample,
+    price: rand(10..50),
+    description: "Best raclette in town!"
+  )
+end
+
+puts "seeds créés"
