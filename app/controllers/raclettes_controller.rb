6f675ee174af89047e37afccaed0c9bf5421850d
alban bengounia <alban.bengounia@gmail.com>
2024-12-04 14:58:15 +0100

    added edit update my_raclette and delete to raclette_controller

--- app/controllers/raclettes_controller.rb
+++ app/controllers/raclettes_controller.rb
@@ -2,4 +2,34 @@ class RaclettesController < ApplicationController
   def index
     @raclettes = Raclette.all
   end
+
+  def edit
+    @raclette = Raclette.find(params[:id])
+  end
+
+  def my_raclettes
+    @raclettes = current_user.raclettes
+  end
+
+  def update
+    @raclette = Raclette.find(params[:id])
+
+    if @raclette.update(raclette_params)
+      redirect_to my_raclettes_path, notice: "mise à jour effectuée !"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @raclette = Raclette.find(params[:id])
+    @raclette.destroy
+    redirect_to my_raclettes_path, notice: "Raclette supprimée"
+  end
+
+  private
+
+  def raclette_params
+    params.require(:raclette).permit(:type, :capacity, :description, :price)
+  end
 end
